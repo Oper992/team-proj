189606b6b8a30ffa0c8b7bb3260458f009dcd29c
AlexandraKrosnik <alexandra.krosnik@gmail.com>
2021-12-06 14:49:29 +0200

    our-location

--- src/js/modal.js
+++ src/js/modal.js
@@ -12,3 +12,17 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-location-open]'),
+    closeModalBtn: document.querySelector('[data-location-close]'),
+    modal: document.querySelector('[data-location]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
